4494eb06691f9169987277eea3dbff1227d10643
Sam Queen <samqueenbusiness@gmail.com>
2021-03-28 18:41:21 -0700

    Updated port forwarding in app.js

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
    res.sendFile(__dirname + "/"); 
 });
 
-app.listen('3000', function() {
+app.listen(process.env.PORT || 3000, function() {
    console.log('Connected to port 3000'); 
 });
